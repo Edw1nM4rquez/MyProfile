2f5c3b7220d05a38577e207e176913b2fc4d7e91
Edw1nM4rquez <edwi1999@hotmail.com>
2023-09-13 21:13:52 -0500

    Nuevo diseño de la pagina

--- src/app/public/pages/home/home.component.ts
+++ src/app/public/pages/home/home.component.ts
@@ -14,52 +14,52 @@ export class HomeComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.init();
+    // this.init();
   }
 
   
-  async init(): Promise<void> {
-    function init() {
-      const scene = new THREE.Scene();
-      const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-      camera.position.z = 5;
+  // async init(): Promise<void> {
+  //   function init() {
+  //     const scene = new THREE.Scene();
+  //     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  //     camera.position.z = 5;
 
-      const renderer = new THREE.WebGLRenderer({ antialias: true });
-      renderer.setSize(window.innerWidth, window.innerHeight);
-      let contend_div = document.getElementById("containerThree");
+  //     const renderer = new THREE.WebGLRenderer({ antialias: true });
+  //     renderer.setSize(window.innerWidth, window.innerHeight);
+  //     let contend_div = document.getElementById("containerThree");
 
-      contend_div?.appendChild(renderer.domElement);
+  //     contend_div?.appendChild(renderer.domElement);
      
-      // Crear la luna
-      const moonGeometry = new THREE.SphereGeometry(1, 32, 32);
-      const moonTexture = new THREE.TextureLoader().load('./assets/img/luna.jpg'); // Ruta de la imagen de textura de la luna
-      const moonMaterial = new THREE.MeshPhongMaterial({ map: moonTexture });
-      const moon = new THREE.Mesh(moonGeometry, moonMaterial);
-      scene.add(moon);
+  //     // Crear la luna
+  //     const moonGeometry = new THREE.SphereGeometry(1, 32, 32);
+  //     const moonTexture = new THREE.TextureLoader().load('./assets/img/luna.jpg'); // Ruta de la imagen de textura de la luna
+  //     const moonMaterial = new THREE.MeshPhongMaterial({ map: moonTexture });
+  //     const moon = new THREE.Mesh(moonGeometry, moonMaterial);
+  //     scene.add(moon);
 
-      // Agregar una luz para iluminar la luna
-      const light = new THREE.DirectionalLight(0xffffff, 1);
-      light.position.set(2, 1, 1);
-      scene.add(light);
+  //     // Agregar una luz para iluminar la luna
+  //     const light = new THREE.DirectionalLight(0xffffff, 1);
+  //     light.position.set(2, 1, 1);
+  //     scene.add(light);
 
 
-      // Rotar y animar la luna
-      function animate() {
-        requestAnimationFrame(animate);
-        moon.rotation.y += 0.005;
-        renderer.render(scene, camera);
+  //     // Rotar y animar la luna
+  //     function animate() {
+  //       requestAnimationFrame(animate);
+  //       moon.rotation.y += 0.005;
+  //       renderer.render(scene, camera);
 
       
-      }
+  //     }
 
-      animate();
-    }
+  //     animate();
+  //   }
 
-    init();
-   }
+  //   init();
+  //  }
 
    public navigateExperience(){
-      this.router.navigate(['/MyProfile/experience']);
+      this.router.navigate(['/experience']);
    }
 
 }
