acb5fec95edef2c426d320d5a883ca5eacf32d97
Edw1nM4rquez <edwi1999@hotmail.com>
2025-08-13 17:36:46 -0500

    Feat: Adding new changes in my profile.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'MyProfile',
     loadChildren: () =>
       import('./public/public.module').then((m) => m.PublicModule),
   },
   {
     path: '',
-    redirectTo: '',
+    redirectTo: 'MyProfile',
     pathMatch: 'full',
   },
 ];
--- src/app/shared/menu-public/menu-public.component.ts
+++ src/app/shared/menu-public/menu-public.component.ts
@@ -15,7 +15,7 @@ export class MenuPublicComponent implements OnInit {
     this.links = [
       {
         title: 'Inicio',
-        url: '/',
+        url: '/MyProfile',
         svg: this.sanitizer
           .bypassSecurityTrustHtml(` <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l9-9 9 9v8a2 2 0 0 1-2 2h-2a2 2 0 0 1-2-2v-4a2 2 0 0 0-2-2h-2a2 2 0 0 0-2 2v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/>
