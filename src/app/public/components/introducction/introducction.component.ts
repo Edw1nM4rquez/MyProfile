981e4a4066de023db01d72fc8391a1735673e8c6
Edw1nM4rquez <edwi1999@hotmail.com>
2025-11-06 16:40:51 -0500

    feat: Adding new CV in my github projct.

--- src/app/public/components/introducction/introducction.component.ts
+++ src/app/public/components/introducction/introducction.component.ts
@@ -4,8 +4,21 @@ import { Component } from '@angular/core';
   selector: 'app-introducction',
   templateUrl: './introducction.component.html',
   styleUrls: ['./introducction.component.scss'],
-  standalone: true
+  standalone: true,
 })
 export class IntroducctionComponent {
+  
+  downloadCV(): void {
+    const link = document.createElement('a');
+    link.href = '/assets/CV-Edwin-Marquez.pdf';
+    link.download = 'CV-Edwin-Marquez.pdf';
+    link.click();
+  }
 
+  goToWorkSection(): void {
+    const workSection = document.getElementById('work-section');
+    if (workSection) {
+      workSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
--- src/app/public/components/profile-description/profile-description.component.ts
+++ src/app/public/components/profile-description/profile-description.component.ts
@@ -8,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class ProfileDescriptionComponent {
 
+  downloadCV(): void {
+    const link = document.createElement('a');
+    link.href = '/assets/CV-Edwin-Marquez.pdf';
+    link.download = 'CV-Edwin-Marquez.pdf';
+    link.click();
+  }
+
 }
